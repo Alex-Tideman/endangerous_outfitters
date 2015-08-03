5ac347a3d717573e81b6bc8476216a233a135432
Alex Tideman <alex.tideman@gmail.com>
2015-08-03 14:32:51 -0600

    Add user homepage feature tests

--- spec/features/user_sees_homepage_spec.rb
+++ spec/features/user_sees_homepage_spec.rb
@@ -4,6 +4,41 @@ describe 'User features' do
   it 'has a homepage' do
 
     visit root_path
-    expect(page).to have_content("Welcome")
+    expect(page).to have_content("Endangerous Outfitters")
   end
+
+  it 'has a login link' do
+    visit root_path
+
+    expect(page).to have_content("Login")
+  end
+
+  it 'has a register link' do
+    visit root_path
+
+    expect(page).to have_content("Register")
+  end
+
+  it 'has a destinations link' do
+    visit root_path
+
+    expect(page).to have_content("Destinations")
+  end
+
+  it 'has a activities link' do
+    visit root_path
+
+    expect(page).to have_content("Activities")
+  end
+
+  it "can create a user" do
+    visit root_path
+    click_link("Register")
+    fill_in :username, with: "John"
+    fill_in :password, with: 1234
+    click_button("Submit")
+
+    expect(page).to have_content("Welcome, John")
+  end
+
 end

78881a439ad3d801f9371cdead858fb381ae9165
MaxMillington <maxmillington89@gmail.com>
2015-08-03 17:28:26 -0600

    set up destinations index page with cards for each continent

---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,7 @@
+class DestinationsController < ApplicationController
+
+  def index
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  get "/destinations", to: "destinations#index"
 end

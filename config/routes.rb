f2b65dcb51c41da39fced40944a37b573af02099
Alex Tideman <alex.tideman@gmail.com>
2015-08-04 13:32:42 -0600

    Add Cart with Cart table and Checkout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  get '/destinations', to: 'destinations#index'
+
+  get '/destinations', to: 'destinations#index'
+
+  get '/cart', to: 'cart#show'
 end

c8a7aafd97c6b3663e2040da160416fb587c70ef
dsshim <daveshim@gmail.com>
2015-08-04 16:59:11 -0600

    added relationships to models

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,6 @@
 class Destination < ActiveRecord::Base
+  validates :continent, :description , presence: true
+  validates :continent, uniqueness: true
+  has_many :trips
+  has_many :activities, through: :trips
 end

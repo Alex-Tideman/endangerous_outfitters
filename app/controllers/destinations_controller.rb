d7673ca5e5e933930f1e3047e402a728bf629a59
MaxMillington <maxmillington89@gmail.com>
2015-08-04 20:20:07 -0600

    added instance variables to show and index methods in the destinations controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,11 +1,24 @@
 class DestinationsController < ApplicationController
 
   def index
-
+    @destinations = Destination.all
+    @africa = Destination.find(5)
+    @asia = Destination.find(4)
+    @oceania = Destination.find(6)
+    @north_america = Destination.find(1)
+    @south_america = Destination.find(2)
+    @europe = Destination.find(3)
   end
 
   def show
-
+    @africa = Destination.find(5)
+    @asia = Destination.find(4)
+    @oceania = Destination.find(6)
+    @north_america = Destination.find(1)
+    @south_america = Destination.find(2)
+    @europe = Destination.find(3)
   end
+
+
 
 end

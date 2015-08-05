a474e182ec2b3f0ac5d6c678c5218ec2c1a04086
Adam Caron <adamwcaron@gmail.com>
2015-08-05 10:52:06 -0600

    adds test for extra's trips

--- spec/models/extra_spec.rb
+++ spec/models/extra_spec.rb
@@ -1,31 +1,29 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Extra, type: :model do
   let(:extra) { Extra.new(name: "Rhino Jousting", description: "Gear up, boy.", cost: 10000.00) }
 
-  it 'is valid' do
+  it "is valid" do
     expect(extra).to be_valid
   end
 
-  it 'is invalid without a name' do
+  it "is invalid without a name" do
     extra.name = nil
     expect(extra).to_not be_valid
   end
 
-  it 'is invalid without a description' do
+  it "is invalid without a description" do
     extra.description = nil
     expect(extra).to_not be_valid
   end
 
-  it 'is invalid without a cost' do
+  it "is invalid without a cost" do
     extra.cost = nil
     expect(extra).to_not be_valid
   end
 
-  # it 'has an array of trips that it belongs to' do
-  #   expect(extra.trips).to eq([])
-  # end
-  # has_many :trip_extras
-  # has_many :trips, through: :trip_extras
+  it "has an array of trips" do
+    expect(extra.trips).to eq([])
+  end
 end
 

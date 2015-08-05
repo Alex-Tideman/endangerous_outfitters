b3ec336e42aaeae6587577aa953d3d2ce0726b78
MaxMillington <maxmillington89@gmail.com>
2015-08-04 19:55:59 -0600

    added unit tests for the destinations model

--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Destination, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:destination) { Destination.new(continent: "Africa", description: "Over there", lat: 5.5, long: 6.6) }
+
+  it 'is valid' do
+    expect(destination).to be_valid
+  end
+
+  it 'is invalid without a continent' do
+    destination.continent = nil
+    expect(destination).to_not be_valid
+  end
+
+  it 'is invalid without a description' do
+    destination.description = nil
+    expect(destination).to_not be_valid
+  end
+
+  it 'has an array of trips' do
+    expect(destination.trips).to eq([])
+  end
+
+  it 'has an array of activities' do
+    expect(destination.activities).to eq([])
+  end
+
+
+
+
 end

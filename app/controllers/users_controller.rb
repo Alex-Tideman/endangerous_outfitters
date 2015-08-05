0c691537c3b09e1e5d91bbe634029fd4e8e6ba97
Alex Tideman <alex.tideman@gmail.com>
2015-08-05 14:01:49 -0600

    Add correct login, new user, and logout paths to nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,18 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to @user
+      flash[:notice] = "Welcome back #{@user.username}!"
+      redirect_to root_path
     else
       flash[:error] = "Invalid input - Please try creating user again"
       render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 
 end

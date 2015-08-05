55fda1481f3e050ebc4485095a7b512a19f8ac5e
MaxMillington <maxmillington89@gmail.com>
2015-08-05 15:21:22 -0600

    add sample data to cart feature test

--- spec/features/visitor_adds_trip_to_cart_spec.rb
+++ spec/features/visitor_adds_trip_to_cart_spec.rb
@@ -5,13 +5,20 @@ feature "Visitor adds a trip to cart" do
   # test cart link works: homepage >     click_link "Cart"
 
   scenario "not logged in with an empty cart" do
-    trip = Trip.create(name: "Canadian Rockies", trip_cost: 20000.00, total_cost: 20000.00)
+    activity = Activity.create(name: "Hiking", description: "Move your legs")
+    destination = Destination.create(continent: "Africa",
+                                     description: "The second biggest continent" )
+    trip = Trip.create(destination_id: destination.id, activity_id: activity.id,
+    total_cost: 20000.00, trip_cost: 20000.00, name: "High Atlas")
+
+
+
 
     visit trip_path(trip)
     click_button("Book Trip")
     expect(current_path).to eq(cart_path)
 
-    expect(page.body).to have_content("Canadian Rockies")
+    expect(page.body).to have_content("High Atlas")
     expect(page.body).to have_content("1")
   end
 

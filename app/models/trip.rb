a7b72ae46d9730605f56feae8f401b41046a8292
Alex Tideman <alex.tideman@gmail.com>
2015-08-05 11:40:27 -0600

    Add more model tests that test total cost when extras are added - WIP

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,14 @@
 class Trip < ActiveRecord::Base
-  validates :trip_cost, :total_cost, presence: true
+  validates :trip_cost, :total_cost, :name, presence: true
   belongs_to :destination
   belongs_to :activity
   has_many :trip_extras
   has_many :extras, through: :trip_extras
+
+  def self.total_cost
+    self.extras.each do |extra|
+      self.update_attribute(total_cost: + extra.cost)
+    end
+  end
+
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -2,16 +2,88 @@ require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
 
-  describe 'Trip model' do
+  context '#trips' do
     it 'has a name' do
+      trip = create_trip
 
+      expect(trip.name).to eq("Machu Picchu")
+    end
+
+    it 'has an trip cost' do
+      trip = create_trip
+      expect(trip.trip_cost).to eq(5000.00)
+    end
+
+    it 'has an total cost' do
+      trip = create_trip
+      expect(trip.trip_cost).to eq(5000.00)
+    end
+
+    it 'has an activity name' do
+      trip = create_trip
+      expect(trip.activity.name).to eq("Hiking")
+    end
+
+    it 'has an destination continent' do
+      trip = create_trip
+      expect(trip.destination.continent).to eq("South America")
+    end
+
+    it "does not create an trip with name missing" do
       destination = Destination.create(continent: "South America", description: "South American description",
                                        lat: -10.23, long: 32.393)
       activity = Activity.create(name: "Hiking", description: "Move your legs")
-      trip = Trip.create(destination_id: destination.id, activity_id: activity.id, name: "Machu Picchu", trip_cost: 5000.00, total_cost: 5000.00 )
+      trip = Trip.create(destination_id: destination.id, activity_id: activity.id, trip_cost: 5000.00, total_cost: 5000.00 )
 
-      expect(trip).to have_content("Hike in South America")
+      expect(Trip.all.count).to eq(0)
     end
+
+    it "does not create an trip with trip cost missing" do
+      destination = Destination.create(continent: "South America", description: "South American description",
+                                       lat: -10.23, long: 32.393)
+      activity = Activity.create(name: "Hiking", description: "Move your legs")
+      trip = Trip.create(destination_id: destination.id, activity_id: activity.id, name: "Machu Picchu", total_cost: 5000.00 )
+
+      expect(Trip.all.count).to eq(0)
+    end
+
+    it "does not create an trip with total cost missing" do
+      destination = Destination.create(continent: "South America", description: "South American description",
+                                       lat: -10.23, long: 32.393)
+      activity = Activity.create(name: "Hiking", description: "Move your legs")
+      trip = Trip.create(destination_id: destination.id, activity_id: activity.id, name: "Machu Picchu", trip_cost: 5000.00 )
+
+      expect(Trip.all.count).to eq(0)
+    end
+
+    it "it can add one extra" do
+      trip = create_trip
+      trip.extras.create(name: "Sherpa", description: "Carries your things", cost: 2500.00)
+
+      expect(trip.extras.count).to eq(1)
+      expect(trip.extras.first.name).to eq("Sherpa")
+      expect(trip.total_cost).to eq(7500.00)
+    end
+
+    it "it can add multiple extra" do
+      trip = create_trip
+      trip.extras.create(name: "Sherpa", description: "Carries your things", cost: 2500.00)
+      trip.extras.create(name: "TV", description: "Watch television", cost: 8000.00)
+      trip.extras.create(name: "Gouda", description: "Cheese", cost: 450.00)
+
+      expect(trip.extras.count).to eq(1)
+      expect(trip.extras.first.name).to eq("Sherpa")
+      expect(trip.total_cost).to eq(7500.00)
+    end
+
   end
+
+  def create_trip
+    destination = Destination.create(continent: "South America", description: "South American description",
+                                     lat: -10.23, long: 32.393)
+    activity = Activity.create(name: "Hiking", description: "Move your legs")
+    Trip.create(destination_id: destination.id, activity_id: activity.id, name: "Machu Picchu", trip_cost: 5000.00, total_cost: 5000.00 )
+  end
+
 
 end

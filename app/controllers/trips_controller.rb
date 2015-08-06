58bfc417e984138bea1d376e5f80dfcfa9f16bd5
MaxMillington <maxmillington89@gmail.com>
2015-08-06 14:08:08 -0600

    added a trips index page that shows cards containing names and photos of all trips.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,6 +4,9 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id].to_i)
   end
 
+  def index
+    @trips = Trip.all
+  end
     # WIP - update total cost of trip when there are extras
     # trip.extras.each { |extra| trip.total_cost += extra.cost } if trip.extras
 

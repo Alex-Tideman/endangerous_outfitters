b8c0bc93c18e859f7a483c216ea6516c42fe3f98
MaxMillington <maxmillington89@gmail.com>
2015-08-06 14:30:47 -0600

    added a description column to the trips table so that we can have descriptions on each trip's show page. Similarly updated seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806023040) do
+ActiveRecord::Schema.define(version: 20150806202024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20150806023040) do
     t.datetime "updated_at",     null: false
     t.text     "name"
     t.string   "image_url"
+    t.text     "description"
   end
 
   add_index "trips", ["activity_id"], name: "index_trips_on_activity_id", using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,23 +20,32 @@ Activity.create(name: "Base Jumping", description: "WHOOO.", image_url: "basebas
 Activity.create(name: "Hunting", description: "BANG.", image_url: "hunting.jpg" )
 
 Trip.create(destination_id: 1, activity_id: 1, total_cost: 20000.00, trip_cost: 10000.00,
-            name: "Canadian Rockies", image_url: "canadian-rockies.jpg" )
+            name: "Canadian Rockies", image_url: "canadian-rockies.jpg",
+            description: "Navigate the trails in Alberta and British Columbia.")
 Trip.create(destination_id: 6, activity_id: 3, total_cost: 21000.00, trip_cost: 21000.00,
-            name: "Great Barrier Reef", image_url: "barrier.jpg")
+            name: "Great Barrier Reef", image_url: "barrier.jpg",
+            description: "Dive deep and experience on of the most colorful places on the planet.")
 Trip.create(destination_id: 5, activity_id: 6, total_cost: 100000.00, trip_cost: 200000.00,
-            name: "African Safari", image_url: "safari.jpg" )
+            name: "African Safari", image_url: "safari.jpg",
+            description: "Fulfill your dreams and capture the biggest game.")
 Trip.create(destination_id: 3, activity_id: 5, total_cost: 20000.00, trip_cost: 15000.00,
-            name: "Swiss Base Jumpers", image_url: "base.jpg" )
+            name: "Swiss Base Jumpers", image_url: "base.jpg",
+            description: "Take a gamble and dive off the cliffs of Lauterbrunnen.")
 Trip.create(destination_id: 4, activity_id: 4, total_cost: 10000.00, trip_cost: 8000.00,
-            name: "Climbing in Pakistan", image_url: "climbo.jpg" )
+            name: "Climbing in Pakistan", image_url: "climbo.jpg",
+            description: "Summit some of the most beautiful and untouched mountains on Earth.")
 Trip.create(destination_id: 2, activity_id: 1, total_cost: 400000.00, trip_cost: 15000.00,
-            name: "Patagonia", image_url: "argentina.jpg" )
+            name: "Patagonia", image_url: "argentina.jpg",
+            description: "Travel to the end of the Earth and experience the strange beauty of Patagonia.")
 Trip.create(destination_id: 1, activity_id: 2, total_cost: 10000.00, trip_cost: 8000.00,
-            name: "Riding the Colorado Rapids", image_url: "river.jpg" )
+            name: "Riding the Colorado Rapids", image_url: "river.jpg",
+            description: "Check an item off your bucket list with this once in a lifetime trip down the Colorado River.")
 Trip.create(destination_id: 6, activity_id: 5, total_cost: 30000.00, trip_cost: 25000.00,
-            name: "Base Jumping the Fjords", image_url: "basez.jpg" )
+            name: "Base Jumping the Fjords", image_url: "basez.jpg",
+            description: "Performing the most daring of stunts in the most isolated of places.")
 Trip.create(destination_id: 3, activity_id: 1, total_cost: 117000.00, trip_cost: 6000.00,
-            name: "Hut to Hut in the Dolomites", image_url: "dolomites.jpg")
+            name: "Hut to Hut in the Dolomites", image_url: "dolomites.jpg",
+            description: "Expericne the old world culture of Northern Italy.")
 
 Extra.create(name: "Monkey Banana-Eating Contest", description: "description", cost: 2000.00)
 Extra.create(name: "Shark Slalom Surfing", description: "description", cost: 4000.00)

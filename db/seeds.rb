c0cce2581c353cd31959cfdc482be9fd91438420
MaxMillington <maxmillington89@gmail.com>
2015-08-06 14:15:11 -0600

    changed seed data from Himalayas to Pakistan so that all the cards would be the same size

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,24 @@ Activity.create(name: "Climbing", description: "bloody hands.", image_url: "clim
 Activity.create(name: "Base Jumping", description: "WHOOO.", image_url: "basebase.jpg" )
 Activity.create(name: "Hunting", description: "BANG.", image_url: "hunting.jpg" )
 
-Trip.create(destination_id: 1, activity_id: 1, total_cost: 20000.00, trip_cost: 10000.00, name: "Canadian Rockies", image_url: "canadian-rockies.jpg" )
-Trip.create(destination_id: 6, activity_id: 3, total_cost: 21000.00, trip_cost: 21000.00, name: "Great Barrier Reef", image_url: "barrier.jpg")
-Trip.create(destination_id: 5, activity_id: 6, total_cost: 100000.00, trip_cost: 200000.00, name: "African Safari", image_url: "safari.jpg" )
-Trip.create(destination_id: 3, activity_id: 5, total_cost: 20000.00, trip_cost: 15000.00, name: "Swiss Base Jumpers", image_url: "base.jpg" )
-Trip.create(destination_id: 4, activity_id: 4, total_cost: 10000.00, trip_cost: 8000.00, name: "Himalayan Mountain Climbing", image_url: "climbo.jpg" )
-Trip.create(destination_id: 2, activity_id: 1, total_cost: 400000.00, trip_cost: 15000.00, name: "Patagonia", image_url: "argentina.jpg" )
-Trip.create(destination_id: 1, activity_id: 2, total_cost: 10000.00, trip_cost: 8000.00, name: "Riding the Colorado Rapids", image_url: "river.jpg" )
-Trip.create(destination_id: 6, activity_id: 5, total_cost: 30000.00, trip_cost: 25000.00, name: "Base Jumping the Fjords", image_url: "basez.jpg" )
-Trip.create(destination_id: 3, activity_id: 1, total_cost: 117000.00, trip_cost: 6000.00, name: "Hut to Hut in the Dolomites", image_url: "dolomites.jpg")
+Trip.create(destination_id: 1, activity_id: 1, total_cost: 20000.00, trip_cost: 10000.00,
+            name: "Canadian Rockies", image_url: "canadian-rockies.jpg" )
+Trip.create(destination_id: 6, activity_id: 3, total_cost: 21000.00, trip_cost: 21000.00,
+            name: "Great Barrier Reef", image_url: "barrier.jpg")
+Trip.create(destination_id: 5, activity_id: 6, total_cost: 100000.00, trip_cost: 200000.00,
+            name: "African Safari", image_url: "safari.jpg" )
+Trip.create(destination_id: 3, activity_id: 5, total_cost: 20000.00, trip_cost: 15000.00,
+            name: "Swiss Base Jumpers", image_url: "base.jpg" )
+Trip.create(destination_id: 4, activity_id: 4, total_cost: 10000.00, trip_cost: 8000.00,
+            name: "Climbing in Pakistan", image_url: "climbo.jpg" )
+Trip.create(destination_id: 2, activity_id: 1, total_cost: 400000.00, trip_cost: 15000.00,
+            name: "Patagonia", image_url: "argentina.jpg" )
+Trip.create(destination_id: 1, activity_id: 2, total_cost: 10000.00, trip_cost: 8000.00,
+            name: "Riding the Colorado Rapids", image_url: "river.jpg" )
+Trip.create(destination_id: 6, activity_id: 5, total_cost: 30000.00, trip_cost: 25000.00,
+            name: "Base Jumping the Fjords", image_url: "basez.jpg" )
+Trip.create(destination_id: 3, activity_id: 1, total_cost: 117000.00, trip_cost: 6000.00,
+            name: "Hut to Hut in the Dolomites", image_url: "dolomites.jpg")
 
 Extra.create(name: "Monkey Banana-Eating Contest", description: "description", cost: 2000.00)
 Extra.create(name: "Shark Slalom Surfing", description: "description", cost: 4000.00)

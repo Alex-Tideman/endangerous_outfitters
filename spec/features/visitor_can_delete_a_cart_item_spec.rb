9c3ca0515b311ecfa15196a1c636610ecee9326a
MaxMillington <maxmillington89@gmail.com>
2015-08-08 08:18:10 -0600

    added a test to confirm that the decrement button works

--- spec/features/visitor_can_delete_a_cart_item_spec.rb
+++ spec/features/visitor_can_delete_a_cart_item_spec.rb
@@ -46,3 +46,28 @@ feature "Visitor adds more than one of the same trip" do
 
 end
 
+feature "Visitor can decrement trip quantity" do
+
+  scenario "loaded cart" do
+    activity = Activity.create(name: "Hiking", description: "Move your legs")
+    destination = Destination.create(continent: "Africa",
+                                     description: "The second biggest continent" )
+    trip = Trip.create(destination_id: destination.id, activity_id: activity.id,
+                       total_cost: 20000.00, trip_cost: 20000.00, name: "High Atlas")
+
+
+    visit trip_path(trip)
+    click_link("Book Trip")
+    expect(current_path).to eq(cart_path)
+
+    expect(page.body).to have_content("High Atlas")
+    expect(page.body).to have_content("1")
+
+    click_button("+")
+    expect(page.body).to have_content("2")
+
+    click_button("-")
+    expect(page.body).to have_content("1")
+  end
+
+end

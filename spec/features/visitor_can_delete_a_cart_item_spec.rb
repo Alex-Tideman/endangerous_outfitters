ad3f096c2cacef357ea505bb8e852adfd9e0633d
MaxMillington <maxmillington89@gmail.com>
2015-08-08 08:17:03 -0600

    added a test to confirm that the increment button on the cart page works

--- spec/features/visitor_can_delete_a_cart_item_spec.rb
+++ spec/features/visitor_can_delete_a_cart_item_spec.rb
@@ -22,3 +22,27 @@ feature "Visitor deletes a trip" do
   end
 
 end
+
+feature "Visitor adds more than one of the same trip" do
+
+  scenario "loaded cart" do
+    activity = Activity.create(name: "Hiking", description: "Move your legs")
+    destination = Destination.create(continent: "Africa",
+                                     description: "The second biggest continent" )
+    trip = Trip.create(destination_id: destination.id, activity_id: activity.id,
+                       total_cost: 20000.00, trip_cost: 20000.00, name: "High Atlas")
+
+
+    visit trip_path(trip)
+    click_link("Book Trip")
+    expect(current_path).to eq(cart_path)
+
+    expect(page.body).to have_content("High Atlas")
+    expect(page.body).to have_content("1")
+
+    click_button("+")
+    expect(page.body).to have_content("2")
+  end
+
+end
+

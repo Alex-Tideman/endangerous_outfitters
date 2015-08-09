d2613860fb3d2b8378d789fda2cbe3e12ef1818e
Adam Caron <adamwcaron@gmail.com>
2015-08-09 15:53:09 -0600

    add trip creation form

--- spec/features/admin_creates_trip_spec.rb
+++ spec/features/admin_creates_trip_spec.rb
@@ -9,6 +9,7 @@ feature "Admin features" do
       fill_in("Username", with: "Alex")
       fill_in("Password", with: "password")
       click_button("Login")
+
     end
 
     context "admin dashboard" do
@@ -23,18 +24,20 @@ feature "Admin features" do
 
     context "create-trip page" do
       it "creates a trip with valid params" do
+        Destination.create(continent: "Asia", description: "description",
+                    lat: 31.8369, long: -77.6847, image_url: "asia.jpg" )
+        Activity.create(name: "Hiking", description: "We loven to do ze hikes.", image_url: "hiker.jpg" )
+
         visit admin_dashboard_path
         click_link "Create a New Trip"
-
-        fill_in "Trip Name", with: "Awesome Trip of Awesome"
-        fill_in "Description", with: "The best trip ever. Here's a description."
-        fill_in "Trip Cost", with: 20000.00
-        Trip.create(destination_id: 1, activity_id: 1, total_cost: 20000.00, trip_cost: 10000.00,
-            name: "Canadian Rockies", image_url: "canadian-rockies.jpg",
-            description: "Navigate the trails in Alberta and British Columbia.")
-        select("North America", from: "Destination")
-        select("Hiking", from: "Activity")
-        attach_file("Trip Image", "/path/to/file.png")
+        fill_in "Trip Name:", with: "Awesome Trip of Awesome"
+        fill_in "Description:", with: "The best trip ever. Here's a description."
+        fill_in "Price:", with: 20000.00
+        select("Asia", from: "destination_id")
+        select("Hiking", from: "activity_id")
+        # page.attach_file("image_url", "hiker.jpg")
+        click_button "Create Trip"
+        expect(current_path).to eq(trips_path)
       end
 
       it "doesn't create a trip with invalid params" do
@@ -49,13 +52,7 @@ feature "Admin features" do
     end
   end
 
-
-
-
   # should be able to create a trip with existing destinations and activities,
   # and also able to create a trip with a new activity.
-
-
-
 
 end

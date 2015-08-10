82696dc097612af69739ed1421b39a27874ce555
Adam Caron <adamwcaron@gmail.com>
2015-08-10 08:52:03 -0600

    adds test for viewing past orders

--- spec/features/user_views_past_orders_spec.rb
+++ spec/features/user_views_past_orders_spec.rb
@@ -2,10 +2,24 @@ require 'rails_helper'
 
 feature "User can view past orders" do
   scenario "User logged in" do
-    it "displays orders belonging to this user"
+    it "displays orders belonging to this user" do
+      user = User.create(username: "adam", password: "adam")
+      visit '/login'
+      fill_in "Username", with: "adam"
+      fill_in "Password", with: "adam"
+      click_button "Login"
+
+      visit '/profile'
+
+      expect(page).to have_content("Orders for adam")
 
       # As an Authenticated User
       # When I visit "/profile"
       # Then I should see all orders belonging to me
+    end
+
+    it "displays orders belonging to this user" do
+      user.orders.create
+    end
   end
 end

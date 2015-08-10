11c1bda93f65ca6aa6322e96079d3d5c333d4873
Adam Caron <adamwcaron@gmail.com>
2015-08-09 19:18:44 -0600

    initial commit, add feature test

---
+++ spec/features/user_views_past_orders_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature "User can view past orders" do
+  scenario "User logged in" do
+    it "displays orders belonging to this user"
+
+      # As an Authenticated User
+      # When I visit "/profile"
+      # Then I should see all orders belonging to me
+  end
+end

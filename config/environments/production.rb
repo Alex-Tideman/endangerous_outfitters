ebe08c5f4098b7ba76ed2f085d504d0b4bdad6ec
Adam Caron <adamwcaron@gmail.com>
2015-08-12 16:36:31 -0600

    add url for s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,8 @@ config.paperclip_defaults = {
      :s3_credentials => {
          :bucket => ENV['AWS_BUCKET'],
          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+         :url => ":s3_domain_url"
      }
  }
 

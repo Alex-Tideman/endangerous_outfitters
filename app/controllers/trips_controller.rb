c6c0f1b7b5cfc731d03e63d5615cea2a01acb9d9
MaxMillington <maxmillington89@gmail.com>
2015-08-13 13:21:34 -0600

    refactored trips controller like there was no tomorrow

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,28 +1,27 @@
 class TripsController < ApplicationController
   def show
     @trip = Trip.find(params[:id].to_i)
-    @hash = Gmaps4rails.build_markers(@trip) do |trip, marker|
-      marker.lat trip.lat
-      marker.lng trip.long
-    end
+    build_google_markers(@trip)
   end
 
   def index
     @trips = Trip.all
-    @hash = Gmaps4rails.build_markers(@trips) do |trip, marker|
-      marker.lat trip.lat
-      marker.lng trip.long
-      marker.infowindow "<a id='map-links' href='#{trip_url(trip)}'>#{trip.name}</a>"
-    end
+    build_google_markers(@trips)
   end
 
   private
 
   def trip_params
     params.require(:trip).permit(:name, :description, :trip_cost,
-                                 :destination_id, :activity_id, :image, :lat, :long)
+    :destination_id, :activity_id, :image, :lat, :long)
   end
 
-  
+  def build_google_markers(data)
+    @hash = Gmaps4rails.build_markers(data) do |trip, marker|
+      marker.lat trip.lat
+      marker.lng trip.long
+      marker.infowindow "<a id='map-links' href='#{trip_url(trip)}'>#{trip.name}</a>"
+    end
+  end
 
 end
